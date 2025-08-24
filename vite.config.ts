4172d2cc7e267997280d3cf21f5f4b7f9650909c
chocoanish <chocoboyanish566@gmail.com>
2025-08-25 02:30:09 +0530

    Configured Build Configuration and Manifest JSON

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        content: 'src/content.ts',
+      },
+      output: {
+        entryFileNames: `[name].js`,
+      },
+    },
+  },
 })
